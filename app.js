cd31ebafe8c21d5eb043b603089b97dd1c1149df
luciorenovato <github@renovato.com.br>
2020-05-08 03:55:18 -0300

    Second commit

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+function createCard(photo, property_type, name, price) {
+	return `<div class="card">
+				<img id="photo" src="${photo}" alt="photo">
+				<div class="container">
+					<div id="property_type">${property_type}</div>
+					<div id="name">${name}</div>
+					<div id="price">${price}</div>
+				</div>
+			</div>`
+
 var xhr = new XMLHttpRequest();
 xhr.onload = function () {
 	if (xhr.status >= 200 && xhr.status < 300) {
